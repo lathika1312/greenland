20aee30e536840b15c89c629dadef963691a3bb4
lathika <s.lathika1312@gmail.com>
2026-06-13 14:12:26 +0530

    head add

--- about.php
+++ about.php
@@ -1,22 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thennadu Nilam - Buy Land Build Future</title>
+    
+<?php include 'head.php'; ?>
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="style.css">
-
-
-</head>
 <body>
 
 <?php include 'header.php'; ?>
